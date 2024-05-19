1d7e94d9c35cb1855f14953d2162800192e3a36e
oualid bennahia <94925420+bennahiaoualid@users.noreply.github.com>
2024-05-19 14:53:15 +0100

    edit

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 let listNotification = document.querySelectorAll("li");
 
 listNotification.forEach(element => {
-    element.addEventListener("click",event=>{
+  /*   element.addEventListener("click",event=>{
         event.currentTarget.classList.toggle("extended");
         event.currentTarget.classList.add("read");
-    })
+    }) */
+    element.onclick = function(){
+        element.classList.toggle("extended");
+        element.classList.add("read");
+    }
 });
 let markAllRead = document.getElementById("mark-all-read");
 markAllRead.addEventListener("click",event=>{
